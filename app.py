99a95e08a76c4864a355e43c596cad690459fa55
PPdoCrime <143550318+PPdoCrime@users.noreply.github.com>
2024-05-22 11:28:33 -0300

    Update app.py

--- app.py
+++ app.py
@@ -9,7 +9,7 @@ app = Flask(__name__)
 scope = ["https://spreadsheets.google.com/feeds", "https://www.googleapis.com/auth/drive"]
 creds = ServiceAccountCredentials.from_json_keyfile_name("credentials.json", scope)
 client = gspread.authorize(creds)
-sheet = client.open("NomeDaSuaPlanilha").worksheet("Resultados")
+sheet = client.open("Resultados").worksheet("Resultados")
 
 @app.route('/', methods=['GET', 'POST'])
 def index():
